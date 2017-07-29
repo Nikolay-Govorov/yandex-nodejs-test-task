9648857d59ddbcc4e737e272d14afb62dd273b96
Nikolay Govorov <nikolay_govorov@bk.ru>
2017-07-29 14:09:17 +0600

    Write send data

--- index.js
+++ index.js
@@ -94,16 +94,90 @@ function getData() {
   }, {});
 };
 
-function setData(data) {};
+function setData(data) {
+  ['fio', 'email', 'phone'].forEach((inputName) => {
+    if (data[inputName] === undefined) {
+      throw new Error(`Incorrect data. Required record ${inputName}!`);
+    }
+
+    myFormElement[inputName].value = data[inputName];
+  });
+};
+
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getRandomRoute = () => {
+  const routes = ['success', 'error', 'progress'];
+
+  return routes[getRandom(0, routes.length - 1)];
+};
+
+const sendFormData = (route = getRandomRoute()) => {
+  let resolve = null;
+  const readyPromise = new Promise((r) => { resolve = r });
+
+  const send = (localeRoute, reolve) => {
+    fetch(`./fake-api/${route}.json`, {
+      method: 'GET',
+      mode: 'no-cors',
+    })
+      .then(response => response.json())
+      .then((data) => {
+        if (data.status === 'progress') {
+          setTimeout(send.bind(null, getRandomRoute(), resolve), data.timeout); return;
+        }
+
+        if (data.status === 'error') {
+          resolve(({ status: 'error', message: data.reason })); return;
+        }
+
+        resolve(({ status: 'success' }));
+      });
+  };
+
+  send(route, resolve);
+
+  return readyPromise;
+}
+
+const setResponseForm = (addedClass, message) => {
+  myFormElement.classList.add(addedClass);
+
+  resultContainer.innerText = message;
+};
 
 const submit = () => {
-  const { isValid, errorFields } = validate();
+  // Clear form
+  ['success', 'error'].forEach(deletedClass => myFormElement.classList.remove(deletedClass));
 
-  errorFields.forEach((input) => {
-    myFormElement[input].classList.add('error');
+  ['fio', 'email', 'phone'].forEach((inputName) => {
+    myFormElement[inputName].classList.remove('error');
   });
 
-  console.log('Submit');
+  myFormElement.submitButton.removeAttribute('disabled');
+
+  resultContainer.innerText = '';
+
+  // Validate
+  const { isValid, errorFields } = validate();
+
+  errorFields.forEach((inputName) => {
+    myFormElement[inputName].classList.add('error');
+  });
+
+  if (!isValid) {
+    return;
+  }
+
+  // Send data
+  myFormElement.submitButton.setAttribute('disabled', '');
+
+  sendFormData()
+    .then(({ status, message = 'Success' }) => {
+      setResponseForm(status, message);
+
+      myFormElement.submitButton.removeAttribute('disabled');
+    });
 };
 
 myFormElement.addEventListener('submit', element => (element.preventDefault(), submit()));
