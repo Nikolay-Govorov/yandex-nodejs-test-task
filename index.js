3127b2089d0f6b879c12bee32160b14b08cb6951
Nikolay Govorov <nikolay_govorov@bk.ru>
2017-07-29 15:47:37 +0600

    Move validators from Input class

--- index.js
+++ index.js
@@ -7,9 +7,105 @@
     return routes[getRandom(0, routes.length - 1)];
   };
 
+  class Input {
+    constructor(DOMElement) {
+      this.element = DOMElement;
+    }
+
+    isValid() {}
+  };
+
+  class FIOInput extends Input {
+    isValid() {
+      const value = this.element.value;
+
+      const fio = value.trim();
+
+      const words = fio.split(' ').filter(el => el !== '');
+
+      if (words.length !== 3) {
+        return false;
+      }
+
+      const isValidWorlds = words.reduce((isValid, word) => {
+        const wordPattern = /^[a-zа-яё]+$/i
+
+        if (!word || !wordPattern.test(word)) {
+          return false;
+        }
+
+        return isValid;
+      }, true);
+
+      return isValidWorlds;
+    }
+  }
+
+  class EmailInput extends Input {
+    isValid() {
+      const value = this.element.value;
+
+      if (value.match(/ /ig) !== null) {
+        return false;
+      }
+
+      const [address, host, ...unnecessary] = value.split('@');
+
+      if (!address || !host || unnecessary.length > 0) {
+        return false;
+      }
+
+      const allowedHosts = [
+        'ya.ru', 'yandex.ru', 'yandex.ua', 'yandex.by', 'yandex.kz', 'yandex.com',
+      ];
+
+      if (!allowedHosts.includes(host)) {
+        return false;
+      }
+
+      const illegalСharacters = /[\\\/\"\'\:\;(),]/ig;
+
+      if (address.match(illegalСharacters) !== null) {
+        return false;
+      }
+
+      return true;
+    }
+  }
+
+  class PhoneInput extends Input {
+    isValid() {
+      const value = this.element.value;
+
+      const phonePattern = /^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/i;
+
+      if (!phonePattern.test(value)) {
+        return false;
+      }
+
+      const summNumbers = value
+        .replace(/\D+/ig, '')
+        .split('')
+        .map(number => parseInt(number, 10))
+        .reduce((summ, number) => summ + number, 0);
+
+      if (summNumbers > 30) {
+        return false;
+      }
+
+      return true;
+    }
+  }
+
   class Form {
     constructor() {
       this.myFormElement = document.getElementById('myForm');
+
+      this.inputs = {
+        fio: new FIOInput(this.myFormElement.fio),
+        email: new EmailInput(this.myFormElement.email),
+        phone: new PhoneInput(this.myFormElement.phone),
+      };
 
       this.myFormElement.addEventListener('submit', element => (element.preventDefault(), this.submit()));
     }
@@ -49,81 +145,9 @@
     }
 
     validate() {
-      const validators = {
-        fio(value) {
-          const fio = value.trim();
-
-          const words = fio.split(' ').filter(el => el !== '');
-
-          if (words.length !== 3) {
-            return false;
-          }
-
-          const isValidWorlds = words.reduce((isValid, word) => {
-            const wordPattern = /^[a-zа-яё]+$/i
-
-            if (!word || !wordPattern.test(word)) {
-              return false;
-            }
-
-            return isValid;
-          }, true);
-
-          return isValidWorlds;
-        },
-
-        email(value) {
-          if (value.match(/ /ig) !== null) {
-            return false;
-          }
-
-          const [address, host, ...unnecessary] = value.split('@');
-
-          if (!address || !host || unnecessary.length > 0) {
-            return false;
-          }
-
-          const allowedHosts = [
-            'ya.ru', 'yandex.ru', 'yandex.ua', 'yandex.by', 'yandex.kz', 'yandex.com',
-          ];
-
-          if (!allowedHosts.includes(host)) {
-            return false;
-          }
-
-          const illegalСharacters = /[\\\/\"\'\:\;(),]/ig;
-
-          if (address.match(illegalСharacters) !== null) {
-            return false;
-          }
-
-          return true;
-        },
-
-        phone(value) {
-          const phonePattern = /^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/i;
-
-          if (!phonePattern.test(value)) {
-            return false;
-          }
-
-          const summNumbers = value
-            .replace(/\D+/ig, '')
-            .split('')
-            .map(number => parseInt(number, 10))
-            .reduce((summ, number) => summ + number, 0);
-
-          if (summNumbers > 30) {
-            return false;
-          }
-
-          return true;
-        },
-      };
-
-      const errorFields = ['fio', 'email', 'phone'].reduce((errorFields, input) => {
+      const errorFields = Object.keys(this.inputs).reduce((errorFields, input) => {
         const inputValue = this.myFormElement[input].value;
-        const isValidInput = validators[input](inputValue);
+        const isValidInput = this.inputs[input].isValid();
 
         if (!isValidInput) {
           errorFields.push(input);
@@ -155,7 +179,7 @@
       // Clear form
       ['success', 'error'].forEach(deletedClass => this.myFormElement.classList.remove(deletedClass));
 
-      ['fio', 'email', 'phone'].forEach((inputName) => {
+      Object.keys(this.inputs).forEach((inputName) => {
         this.myFormElement[inputName].classList.remove('error');
       });
 
@@ -186,6 +210,7 @@
     }
   };
 
+  // Public methods
   const { validate, getData, setData, submit } = new Form();
 
   window.myForm = { validate, getData, setData, submit };
